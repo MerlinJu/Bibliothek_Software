470b2b7a8d66a917952e78975d5c47260fc766d8
julius <200219031+Julius289@users.noreply.github.com>
2025-02-26 17:06:36 +0100

    2 Konstruktoren in Klasse Medium für Zustände ausgeliehen und nicht ausgeliehen

--- src/Buch.java
+++ src/Buch.java
@@ -1,7 +1,11 @@
 import java.time.LocalDate;
 
 public class Buch extends Medium {
-    public Buch(String titel, String autor, String standplatz, LocalDate ausleihe_datum) {
-        super(titel, autor, standplatz, Medientyp.BUCH, ausleihe_datum);
+    public Buch(String titel, String autor, LocalDate ausleihe_datum, LocalDate rueckgabe_datum) {
+        super(titel, autor, Medientyp.BUCH, ausleihe_datum, rueckgabe_datum);
+    }
+
+    public Buch(String titel, String autor, String standplatz) {
+        super(titel, autor, standplatz, Medientyp.BUCH);
     }
 }
--- src/Medium.java
+++ src/Medium.java
@@ -8,28 +8,37 @@ public class Medium {
     public String standplatz;
     public Medientyp medientyp;
     public LocalDate ausleihe_datum;
-    // Hinweis, ich habe das Rückgabedatum komplett entfernt, da es unerklärliche Fehler damit gab
+    public LocalDate rueckgabe_datum;
 
-    public Medium(String titel, String autor, String standplatz, Medientyp medientyp,
-                  LocalDate ausleihe_datum) {
+    // Konstruktor für nicht ausgeliehenes Medium (ohne Standplatz)
+    public Medium(String titel, String autor, Medientyp medientyp, LocalDate ausleihe_datum, LocalDate rueckgabe_datum) {
+        this.titel = titel;
+        this.autor = autor;
+        this.standplatz = null;
+        this.medientyp = medientyp;
+        this.ausleihe_datum = ausleihe_datum;
+        this.rueckgabe_datum = rueckgabe_datum;
+    }
+
+    //Konstruktor für ausgeliehenes Medium (ohne Ausleih- und Rückgabedatum)
+    public Medium(String titel, String autor, String standplatz, Medientyp medientyp){
         this.titel = titel;
         this.autor = autor;
         this.standplatz = standplatz;
         this.medientyp = medientyp;
-        this.ausleihe_datum = ausleihe_datum;
-        this.rueckgabe_datum = rueckgabe_datum; // HINWEIS, wir behalten dass hier,
-        // aber adden die 30 tage immer nur in den Bibliothek methoden damit die 2 daten 
-        // hier auch null sein können
+        this.ausleihe_datum = null;
+        this.rueckgabe_datum = null;
     }
-
 
     @Override
     public String toString() {
       
-        return titel + ";" + autor + ";" + standplatz + ";" + medientyp + ";" +
-                (ausleihe_datum != null ? ausleihe_datum : "null") + ";" +
-                (rueckgabe_datum != null ? rueckgabe_datum : "null");
-
+        return  titel +
+                ";" + autor +
+                (standplatz != null ? ";" + standplatz: "") +
+                ";" + medientyp +
+                (ausleihe_datum != null ? ";" + ausleihe_datum: "") +
+                (rueckgabe_datum != null ? ";" + rueckgabe_datum : "");
     }
 
 }
